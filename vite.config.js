1a1b4b67285f88a9fe260fc7aa88434ab57e89d7
fathima <171223302+fathimassrambikkal@users.noreply.github.com>
2025-08-05 20:25:45 +0530

    Remove GitHub Pages base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+<<<<<<< Updated upstream
   
+=======
+
+>>>>>>> Stashed changes
   plugins: [react()],
 
 })
